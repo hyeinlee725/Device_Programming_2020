1d37e0f4920f41578527e43b38cc56e87e9c319e
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2021-01-13 18:17:37 +0900

    Upload LAB08 Datapath VHDL Code-Change Instruction

--- LAB08/Code/lab_08_dp.vhd
+++ LAB08/Code/lab_08_dp.vhd
@@ -1,43 +1,41 @@
-
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 Entity lab_08_dp is
-
-	port(
-			clk    : IN STD_LOGIC ;
-			load    : IN STD_LOGIC ;
-			clear    : IN STD_LOGIC ;
-			out_sel	: IN std_logic ;
-			
-			iNOT10 	: out std_logic;
-			dp_out	: out std_logic_vector(3 downto 0)
-			
-			);
-
+   port(
+         clk    : IN STD_LOGIC ;
+         load    : IN STD_LOGIC ;
+         clear    : IN STD_LOGIC ;
+         out_sel   : IN std_logic ;
+         
+         iNOT10    : out std_logic;
+         dp_out   : out std_logic_vector(3 downto 0)
+         
+         );
 END lab_08_dp;
 
 Architecture dataflow of lab_08_dp is
 
-	signal data : std_logic_vector(3 downto 0) := "0000"; --initialize
-	begin
-		
-		process(clear, clk) is --register i(clocked process)
-			begin 
-			
-			if (clear = '1') then
-           			 data <= "0000"; --i initialize
-			
-			elsif (clk'EVENT AND clk = '1') then
-            			  if (load = '1') then
-				data <= data + '1'; --adder
-			  end if;		
-			end if;	
-		end process;
-			
-		iNOT10 <= '0' when (data = "1010") else '1'; --combinational logic
-		dp_out <= data when (out_sel = '1') else "ZZZZ";
-		
+   signal data : std_logic_vector(3 downto 0) := "0000"; --initialize
+   begin
+      process(clear, clk) is --register i(clocked process)
+         begin 
+         if (clear = '1') then
+              data <= "0000"; --i initialize   
+         elsif (clk'EVENT AND clk = '1') then
+            if (load = '1' AND data /= "1010") then
+               data <= data + '1'; --adder
+               iNOT10 <= '1';
+            else
+               iNOT10 <= '0';
+            end if;
+            IF (out_sel = '1') THEN
+               dp_out <= data; -- output
+            ELSE
+               dp_out <= "ZZZZ";
+            END IF;
+         end if;
+      end process;
 end dataflow;
